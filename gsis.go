8a055405864209828c5f826c7c34197fc4b9d564
Chris <chris@epf.ai>
2021-03-11 11:42:41 -0500

    move is-actual-play logic to method and add functions for abbr <-> club code conversions (#12)

---
+++ gsis.go
@@ -0,0 +1,39 @@
+package gsis
+
+var gsisClubCodeToCommonAbbreviation = map[string]string{
+	"LA":  "LAR",
+	"BLT": "BAL",
+	"HST": "HOU",
+	"CLV": "CLE",
+	"ARZ": "ARI",
+}
+
+// Returns the team's abbreviation based on the club code found in the GSIS API. The abbreviation is
+// the common one, which is not necessarily the official one. These follow most consistent rules:
+//
+// * Single-word markets use the first three letters of the market (e.g. "ARI" for Arizona).
+// * Multi-word markets use an initialism of the market (e.g. "GB" for Green Bay).
+// * Where necessary, the first letter of the team's nickname disambiguates (e.g. "NYG" and "NYJ").
+// * The Jaguars are an exception and are "JAX".
+func CommonTeamAbbreviation(clubCode string) string {
+	if abbr, ok := gsisClubCodeToCommonAbbreviation[clubCode]; ok {
+		return abbr
+	}
+	return clubCode
+}
+
+var abbreviationToGSISClubCode = map[string]string{}
+
+func init() {
+	for k, v := range gsisClubCodeToCommonAbbreviation {
+		abbreviationToGSISClubCode[v] = k
+	}
+}
+
+// Inverts CommonTeamAbbreviation.
+func TeamClubCode(abbr string) string {
+	if code, ok := abbreviationToGSISClubCode[abbr]; ok {
+		return code
+	}
+	return abbr
+}
