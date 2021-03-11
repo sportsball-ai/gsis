52fada1298221cb3a141ffb7d632278fb19a3492
Christopher Brown <ccbrown112@gmail.com>
2021-03-11 02:33:32 -0500

    fix comment typo

--- gsis.go
+++ gsis.go
@@ -12,7 +12,7 @@ var gsisClubCodeToCommonAbbreviation = map[string]string{
 // the common one, which is not necessarily the official one. These follow most consistent rules:
 //
 // * Single-word markets use the first three letters of the market (e.g. "ARI" for Arizona).
-// * Multi-word markets use an initialism of the market (e.g. "GB" for Green Bar).
+// * Multi-word markets use an initialism of the market (e.g. "GB" for Green Bay).
 // * Where necessary, the first letter of the team's nickname disambiguates (e.g. "NYG" and "NYJ").
 // * The Jaguars are an exception and are "JAX".
 func CommonTeamAbbreviation(clubCode string) string {
